8c79553875ff062b164c74bf0260e14c3ed8f9df
Mojahedul Hoque Abul Hasanat <masum@dsinnovators.com>
2021-05-25 10:50:13 +0600

    Simple add/remove

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
-const selectedCountries = [
-  "Afghanistan",
-  "Guyana",
-]
+import { useState } from "react";
 
 const allCountries = [
   "Cambodia",
@@ -14,11 +10,33 @@ const allCountries = [
   "Mauritania",
 ]
 
-function ActionItem({label, action}) {
-  return <li>{label} <a href="#" onClick={e => e.preventDefault()} style={{float: "right", marginLeft: "1rem"}}>{action}</a></li>
+function ActionItem({label, action, handleChange}) {
+  return (
+    <li>{label}
+      <a href="#"
+         onClick={function (e) {
+           e.preventDefault()
+           handleChange(label)
+         }}
+         style={{float: "right", marginLeft: "1rem"}}>
+        {action}
+      </a>
+    </li>
+  )
 }
 
 export default function Home() {
+  const [selectedCountries, setSelectedCountries] = useState([])
+
+  function handleAdd(country) {
+    setSelectedCountries(selectedCountries.concat(country))
+  }
+
+  function handleRemove(country) {
+    const updatedList = selectedCountries.filter(c => c !== country)
+    setSelectedCountries(updatedList)
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -36,14 +54,16 @@ export default function Home() {
           <div className={styles.card}>
             <h2>Selected</h2>
             <ul>
-              {selectedCountries.map(c => <ActionItem key={c} label={c} action="-"/>)}
+              {selectedCountries.map(c =>
+                <ActionItem key={c} label={c} action="-" handleChange={handleRemove}/>)}
             </ul>
           </div>
 
           <div className={styles.card}>
             <h2>Choose From</h2>
             <ul>
-              {allCountries.map(c => <ActionItem key={c} label={c} action="+"/>)}
+              {allCountries.map(c =>
+                <ActionItem key={c} label={c} action="+" handleChange={handleAdd}/>)}
             </ul>
           </div>
         </div>
